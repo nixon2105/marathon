fcec31e88b68193419f96c3794f9e31ca6962c4c
nixon2105 <radivilov1991@mail.ru>
2022-02-15 21:45:32 +0300

    feat: add function setColor

--- board/script.js
+++ board/script.js
@@ -5,5 +5,11 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
 
+  square.addEventListener('mouseover', () => setColor(square));
+
   board.append(square);
 }
+
+const setColor = (element) => {
+  element.style.backgroundColor = 'red';
+};
